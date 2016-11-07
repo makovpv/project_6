05c929e1296993494248766d6a5d573a4af0d927
makovpv <makovpv@gmail.com>
2016-11-07 23:25:06 +0800

    test number 3

---
+++ MyMasterPage.master.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MyMasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //if (Request.LogonUserIdentity.Name != "HOME\\Паша" && Request.LogonUserIdentity.Name != "User-DNS\\User")
+        //    Response.Redirect ("~\\default.aspx");
+
+        //form1.Page.Title = Request.LogonUserIdentity.Name;
+
+        if (HttpContext.Current.User.Identity.IsAuthenticated)
+            lblUserName.Text = HttpContext.Current.User.Identity.Name;
+
+    }
+}
---
+++ PlayerMasterPage.master.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MyMasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            object TestID = Request.QueryString["TestID"];
+            if (TestID != null)
+            {
+                TesterDataClassesDataContext dc = new TesterDataClassesDataContext();
+                TestName_lbl.Text = dc.tests.Where(p => p.id == Convert.ToInt16(TestID)).First().name;
+                Page.Title = TestName_lbl.Text;
+            }
+        }
+        
+    }
+}
