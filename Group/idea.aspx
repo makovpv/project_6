9204b385c17bfbeedd4822abcb2b1f7b28e41146
makovpv <makovpv@gmail.com>
2016-11-14 00:34:43 +0800

    idea file manager

---
+++ Group/idea.aspx
@@ -0,0 +1,115 @@
+﻿<%@ Page Title="Идея" Language="C#" AutoEventWireup="true" CodeFile="idea.aspx.cs" Inherits="Group_idea" 
+    MasterPageFile="~/MainBusinessMasterPage.master" %>
+
+<%--<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxToolkit" %>--%>
+<%--<%@ Register TagPrefix="ctr" TagName="lnkFiles" Src="~/Group/LinkedFilesControl.ascx" %>--%>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    
+    <%--<ajaxToolkit:ToolkitScriptManager runat="Server" EnablePartialRendering="true" ID="ScriptManager1" />--%>
+    <script type="text/javascript" language="javascript">
+        function OnConfirmDelete() {
+            if (confirm('Удалить файл?')) return true;
+            return false;
+        }
+    </script>
+
+    <p>
+    <asp:Label ID="lFIO" runat="server"/><br/>
+    <asp:Label ID="lDate" runat="server"/><br/>
+    <asp:Label ID="lDept" runat="server"/><br/>
+    </p>
+
+    <div runat="server" id="divContent">
+        <asp:Repeater DataSourceID="sqlSubjResults" runat="server">
+            <ItemTemplate>
+                <asp:Label runat="server" Text='<%# Eval("header") %>' Font-Bold="true" />
+                <br/>
+                <asp:Label runat="server" Text='<%# Eval("text") %>'  Font-Italic="true"/>
+                <br/><br/>
+            </ItemTemplate>
+        </asp:Repeater>
+    </div>
+
+    <%--<ctr:lnkFiles runat="server" ID="lnkFiles" />--%>
+    <asp:Panel ID="Panel1" BackColor="LightGray" runat="server">
+        <asp:Repeater runat="server" ID="rptFiles">
+            <ItemTemplate>
+                <asp:Label ID="Label2" runat="server" Text='<%# Eval("Name") %>' Width="30%" />
+                <asp:Button runat="server" Text="Скачать" CssClass="menuLinkButton"
+                    CommandArgument='<%# Eval("Name") %>' CommandName="Download" OnCommand="CommandBtn_Click" />
+                <asp:Button runat="server" Text="Удалить" CssClass="menuLinkButton"
+                    CommandArgument='<%# Eval("Name") %>' CommandName="Delete" 
+                    OnCommand="CommandBtn_Click" OnClientClick="if (!OnConfirmDelete()) return false;" />
+
+                <br/>
+            </ItemTemplate>
+        </asp:Repeater>
+        <br/>
+        <asp:HiddenField runat="server" ID="hididea"/>
+        <asp:Label ID="Label3" runat="server" Text="Что бы прикрепить документ" Width="30%"/>
+        <asp:FileUpload runat="server" ID="uplControl"  ToolTip="Добавляет новый файл" />
+        <asp:Button ID="Button2" runat="server" Text="Отправить" CommandName="Upload" OnCommand="CommandBtn_Click"/>
+    </asp:Panel>
+
+    <br/>
+    краткое резюме оценочной комиссии
+    <br/>
+    <asp:TextBox runat="server" ID="tbResume"
+        BorderStyle="Solid" Height="100px" 
+        TextMode="MultiLine" Width="50%" BorderColor="#0033CC"/>
+    <br/>
+
+    <p>
+    <asp:Label ID="Label1" Text="Статус" runat="server" /><br/>
+    <asp:DropDownList ID="ddlState" runat="server"  >
+        <asp:ListItem Value="0">На рассмотрении</asp:ListItem>
+        <asp:ListItem Value="4">Отправить на доработку</asp:ListItem>
+        <asp:ListItem Value="5">Отклонено</asp:ListItem>
+        <asp:ListItem Value="6">Реализовано</asp:ListItem>
+        <asp:ListItem Value="7">К внедрению</asp:ListItem>
+        <asp:ListItem Value="8">Не оценивается</asp:ListItem>
+    </asp:DropDownList>
+
+    <asp:HyperLink ID="lnkEditor" Text="Редактировать" runat="server" />
+    </p>
+
+    <asp:LoginView runat="server">
+        <RoleGroups>
+            <asp:RoleGroup Roles="Expert">
+                <ContentTemplate>
+                    <asp:Button ID="btnSave" runat="server" Text="Сохранить" OnClick="btnSaveClick"/>
+                </ContentTemplate>
+            </asp:RoleGroup>
+        </RoleGroups>
+    </asp:LoginView>
+    
+    
+    <p>
+    <asp:HyperLink NavigateUrl="~/lk2.aspx" Text="Вернуться в Личный Кабинет" runat="server" />
+    <br/>
+    <br/>
+    <asp:HyperLink NavigateUrl="~/Analyse/IdeaList.aspx" Text="База идей" runat="server" />
+    </p>
+
+    <asp:SqlDataSource ID="sqlSubjResults" runat="server" ConnectionString="<%$ ConnectionStrings:tester_dataConnectionString %>" 
+        SelectCommand="SELECT [text], header FROM ( 
+        select txt.[text], i.[text] as header , i.id as item_id, i.number 
+        from test_results_txt txt 
+        inner join items i on i.id = txt.item_id 
+        where txt.Subject_id = @SubjID 
+        union all 
+        select dbo.GetMultyAnswerLine (i.id, ts.id), 
+         i.[text] as header, i.id, i.number 
+		from test_subject ts 
+		INNER join subscaledimension ssd on ssd.test_id = ts.test_id and ssd.dimension_type in (13,1,2)  
+		left join items i on i.dimension_id = ssd.id  
+        where ts.id = @SubjID) q
+        order by number, item_id">
+        <SelectParameters>
+            <asp:Parameter Name="SubjID" Type="Int32" />
+        </SelectParameters>
+    </asp:SqlDataSource>
+</asp:Content>
---
+++ Group/idea.aspx.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class Group_idea : System.Web.UI.Page
+{
+    int ID { get { return Convert.ToInt32(Request.QueryString["id"]); } }
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            using (TesterDataClassesDataContext dc = new TesterDataClassesDataContext())
+            {
+                idea subj_idea = dc.ideas.Where(p => p.id == ID).FirstOrDefault();
+                
+                lFIO.Text = subj_idea.Test_Subject.fio;
+                lDate.Text = subj_idea.Test_Subject.Test_Date.Value.ToShortDateString ();
+                dept user_dept = subj_idea.Test_Subject.user_account.dept;
+                if (user_dept != null)
+                    lDept.Text = subj_idea.Test_Subject.user_account.dept.name;
+                else lDept.Text = "не указан";
+
+                tbResume.Text = subj_idea.resume;
+                ddlState.SelectedValue = subj_idea.idState.ToString();
+                //lnkFiles.idIdea = ID;
+                ReloadFileList();
+
+                sqlSubjResults.SelectParameters["SubjID"].DefaultValue = subj_idea.idSubject.HasValue ? ((int)subj_idea.idSubject.Value).ToString() : "0";
+
+                lnkEditor.Visible = subj_idea.idState == (byte)enIdeaState.istRevision && subj_idea.Test_Subject.idUser == CommonData.GetCurrentUserKey();
+                if (lnkEditor.Visible)
+                    lnkEditor.NavigateUrl = string.Format("~\\group\\ansall.aspx?id={0}", subj_idea.idSubject);
+            }
+        }
+        
+    }
+
+    public enum enIdeaState { istImplemented = 6, istRevision = 4}
+
+    protected void btnSaveClick(object sender, EventArgs e)
+    {
+        //if (lnkFiles.UploadControl.HasFile)
+        //{
+        //    lnkFiles.UploadControl.SaveAs(Server.MapPath (string.Format (@"~\Data\Ideas\{0}_{1}", ID, lnkFiles.UploadControl.FileName)));
+        //}
+        
+        using (TesterDataClassesDataContext dc = new TesterDataClassesDataContext())
+        {
+            idea ide = dc.ideas.Where(i => i.id == ID).FirstOrDefault();
+            if (ide != null)
+            {
+                if ((enIdeaState)ide.idState != enIdeaState.istImplemented &&
+                    ddlState.SelectedValue == ((byte)enIdeaState.istImplemented).ToString())
+                {
+                    ide.implement_date = DateTime.Now; // устанавливаем дату реализации
+                }
+                else
+                    if ((enIdeaState)ide.idState == enIdeaState.istImplemented &&
+                        ddlState.SelectedValue != ((byte)enIdeaState.istImplemented).ToString())
+                    {
+                        ide.implement_date = null; // очищаем дату реализации
+                    }
+
+                ide.idState = Convert.ToByte ( ddlState.SelectedValue);
+                ide.resume = tbResume.Text;
+
+                dc.SubmitChanges();
+            }
+            Response.Redirect("~\\lk2.aspx");
+        }
+    }
+    ////////////////////////////////////////////////////////////
+    private void ReloadFileList()
+    {
+        DirectoryInfo di = new DirectoryInfo(Server.MapPath(@"~\data\ideas\"));
+        FileInfo[] files = di.GetFiles(this.ID.ToString() + "_*.*");
+        rptFiles.DataSource = files;
+        rptFiles.DataBind();
+    }
+    
+    protected void CommandBtn_Click(object sender, CommandEventArgs e)
+    {
+        if (e.CommandName == "Upload")
+        {
+            if (uplControl.HasFile)
+            {
+                uplControl.SaveAs(Server.MapPath(string.Format(@"~\Data\Ideas\{0}_{1}", this.ID.ToString(), uplControl.FileName)));
+                ReloadFileList();
+            }
+        }
+        else
+            if (e.CommandArgument != null)
+            {
+                string fname = e.CommandArgument.ToString();
+
+                if (e.CommandName == "Delete")
+                {
+                    File.Delete(Server.MapPath(@"~\data\ideas\" + fname));
+                    //idIdea = Convert.ToInt32(fname.Substring(0, fname.IndexOf('_')));
+                    ReloadFileList();
+                }
+                else if (e.CommandName == "Download")
+                {
+                    string ContentType = "text/html";
+                    switch ((new FileInfo(e.CommandArgument.ToString())).Extension.ToLower())
+                    {
+                        case ".png":
+                            ContentType = "image/png";
+                            break;
+                        case ".jpeg":
+                            ContentType = "image/jpeg";
+                            break;
+                        case ".pdf":
+                            ContentType = "application/pdf";
+                            break;
+                    }
+
+                    System.Web.HttpResponse response = System.Web.HttpContext.Current.Response;
+                    response.ClearContent();
+                    response.Clear();
+                    response.ContentType = ContentType; // "text/plain";
+                    response.AddHeader("Content-Disposition", "attachment; filename=" + fname + ";");
+                    response.TransmitFile(Server.MapPath(@"~\data\ideas\" + fname));
+                    response.Flush();
+                    response.End();
+                }
+            }
+    }
+
+}
+
+
+
+
